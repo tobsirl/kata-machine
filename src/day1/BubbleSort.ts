b7019d78f1c7e90380a410b8356ce0cadb48bce4
Paul Tobin <tobster111@gmail.com>
2023-05-04 09:45:37 +0100

    Prime kata-machine Bubble

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
+// sort in place algorithm
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
