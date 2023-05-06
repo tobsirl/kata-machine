33a4dae8f848c434d74300c11c377a744b93faaa
Paul Tobin <tobster111@gmail.com>
2023-05-06 10:06:37 +0100

    Prime kata-machine Queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -12,8 +12,29 @@ export default class Queue<T> {
         this.head = this.tail = undefined;
     }
 
-    enqueue(item: T): void {}
-    deque(): T | undefined {}
+    enqueue(item: T): void {
+        this.length++;
+        if (!this.tail) {
+            this.head = this.tail = { value: item } as QueueNode<T>;
+        }
+    }
+
+    // dequeue removes the head node and returns its value
+    deque(): T | undefined {
+        // check if the queue is empty
+        if (!this.head) {
+            return undefined;
+        }
+        this.length--;
+        // save the value of the head node
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+        return head.value;
+    }
+
+    // peek returns the value of the head node
     peek(): T | undefined {
         return this.head?.value;
     }
