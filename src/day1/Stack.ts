c85b9c2629ee90a3b5eae59e3f86af9c062db908
Paul Tobin <tobster111@gmail.com>
2023-05-07 09:15:24 +0100

    Prime kata-machine Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,24 +1,18 @@
 type Node<T> = {
     value: T;
     prev?: Node<T>;
-}
+};
 
 export default class Stack<T> {
     public length: number;
-
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
-    push(item: T): void {
-
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    push(item: T): void {}
+    pop(): T | undefined {}
+    peek(): T | undefined {}
 }
