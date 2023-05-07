d7dfb60f64d285bbb2345a025192a6c81366f450
Paul Tobin <tobster111@gmail.com>
2023-05-07 09:22:39 +0100

    Prime kata-machine Stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -26,8 +26,14 @@ export default class Stack<T> {
     pop(): T | undefined {
         this.length = Math.max(0, this.length - 1);
         if (this.length === 0) {
+            const head = this.head as Node<T>;
             this.head = undefined;
+            return head?.value;
         }
+        const head = this.head as Node<T>;
+        this.head = head.prev;
+
+        return head.value;
     }
 
     peek(): T | undefined {
