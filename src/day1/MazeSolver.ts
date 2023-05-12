f1534862ff6659625b0e1ef33bc8847add1302cd
Paul Tobin <tobster111@gmail.com>
2023-05-12 15:43:29 +0100

    Prime kata-machine Stack

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,36 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+    // 1. base case off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
 
+    // 2. base case hit a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // 3. base case found the end
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // 4. recursive case
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
