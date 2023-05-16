ed38ff4860c3beda23382faa370c047bad8ee8ed
Paul Tobin <tobster111@gmail.com>
2023-05-16 09:27:31 +0100

    Prime kata-machine Quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,14 @@
-export default function quick_sort(arr: number[]): void {
+function quick_sort(arr: number[], low: number, high: number): void {
+ 
+}
 
+function partition(arr:number[], low:number, high:number):number {
+  
+}
+  
+
+  
+
+export default function quick_sort(arr: number[]): void {
+    quick_sort(arr, 0, arr.length - 1);
 }
