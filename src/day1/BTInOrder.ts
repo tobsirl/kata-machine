60243f61c405846806be3ef4ecc1c43276da085e
Paul Tobin <tobster111@gmail.com>
2023-05-21 10:36:02 +0100

    Prime kata-machine Tree

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,18 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) return path; // base case no node
 
+    // recursive case
+    // pre
+
+    // recurse
+    walk(curr.left, path); // left
+    path.push(curr.value);
+    walk(curr.right, path); // right
+
+    // post
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) return path; // base case no node
 
+    // recursive case
+    // pre
+
+    // recurse
+    walk(curr.left, path); // left
+    walk(curr.right, path); // right
+    path.push(curr.value);
+
+    // post
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
