69f294532409b5654d8b805ad1795e92e28f4fb2
Paul Tobin <tobster111@gmail.com>
2023-05-21 10:28:59 +0100

    Prime kata-machine Tree

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) return path; // base case no node
 
+    // recursive case
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path); // left
+    walk(curr.right, path); // right
+
+    // post
+    return path;
+}
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
